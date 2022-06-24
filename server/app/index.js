d01336eb4a153084821f2cf6f795440fdb3babc1
Itamar <itamarshmaya@gmail.com>
2022-06-24 14:13:12 +0300

    add api do pagec

--- server/app/index.js
+++ server/app/index.js
@@ -6,6 +6,7 @@ import * as url from "url";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const clientPublicDirPath = path.join(__dirname, "../../client/build");
+const apiHtmlPath = path.join(__dirname, "../public");
 
 const app = express();
 
@@ -13,8 +14,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(clientPublicDirPath));
+app.use("/api", express.static(apiHtmlPath));
 
 const PORT = process.env.PORT || 5050;
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../../client/build/index.html"));
+// });
 
 app.listen(PORT, (err) => {
   if (err) return console.log(err);
