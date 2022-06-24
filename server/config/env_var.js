719a5291f5972d859aadd41d107d8ac9ba704039
Itamar <itamarshmaya@gmail.com>
2022-06-24 13:39:46 +0300

    heroku upload config

--- server/config/env_var.js
+++ server/config/env_var.js
@@ -1,5 +1,11 @@
 import dotenv from "dotenv";
+import path from "path";
+import * as url from "url";
 
-dotenv.config();
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const CONFIG_DIR_PATH = __dirname;
+const ENV_FILE_PATH = path.join(CONFIG_DIR_PATH, "../.env");
+
+dotenv.config({ path: ENV_FILE_PATH });
 
 export const { MONGO_ATLAS_USERNAME, MONGO_ATLAS_PASSWORD } = process.env;
