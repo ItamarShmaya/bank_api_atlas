4149319aebc3291bfc8f0e8eed268f402de5fe33
Itamar <itamarshmaya@gmail.com>
2022-06-24 13:49:21 +0300

    heroku upload config

--- server/app/index.js
+++ server/app/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "../db/app.js";
+import path from "path";
+import * as url from "url";
 
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const clientPublicDirPath = path.join(__dirname, "../../client/public");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(clientPublicDirPath));
 
 const PORT = process.env.PORT || 5050;
 
